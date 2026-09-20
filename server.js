33869f7a3299fe4fa90e3fb5e4ff33a5d42c33ef
paventhan <paventhanbtech1990@gmail.com>
2026-09-20 18:36:10 +0530

    changes added

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const Razorpay = require("razorpay");
 require("dotenv").config();
 
 const app = express();
-app.use(cors({
-  origin: 'https://retailproject2026.github.io',  //http://localhost:4200
-  credentials: true
-}));
 
 app.use(express.json());
 
