541be3d484be887552f2cb75a85a723d7e02c2cb
paventhan <paventhanbtech1990@gmail.com>
2026-09-20 19:00:05 +0530

    changes added

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors({
-  origin: 'https://retailproject2026.github.io',  //http://localhost:4200
+  origin: [
+    'https://retailproject2026.github.io',
+    'http://localhost:4200'
+  ],
   credentials: true
 }));
 app.use(express.json());
