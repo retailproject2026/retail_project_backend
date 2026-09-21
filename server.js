159ff61015fc9adb833040bb5843823a31a52907
paventhan <paventhanbtech1990@gmail.com>
2026-09-21 21:54:02 +0530

    changes added

--- server.js
+++ server.js
@@ -212,6 +212,6 @@ app.use((error, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
